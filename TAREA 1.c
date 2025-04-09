bbc039ddc19b1f08f67dabdd9893477effaa90a8
PowCow <paulina.munoz.c01@mail.pucv.cl>
2025-04-08 21:10:56 -0400

    Add files via upload

---
+++ TAREA 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    char rut[11] ;
+    int id ;
+    char descripcion[101] ;
+    int prioridad ; // 1. Alta 2. Media 3. Baja
+} datosPaciente ;
+
+int main()
+{
+    char opcion ;
+}  
